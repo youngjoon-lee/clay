cbdf63a1e318a887dadf784dcdbc41ca85b763ba
Youngjoon Lee <taxihighway@gmail.com>
2020-12-19 22:29:46 +0900

    Make it built as a freestanding binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]  // not to use the Rust runtime and crt0
+
+use core::panic::PanicInfo;
+
+// Called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {  // returning the 'never' type
+    loop{}
+}
+
+// Overwrite the entry point
+#[no_mangle] // disabling the name mangling to ensure that Rust compiler really outputs a func name with the name `_start`.
+pub extern "C" fn _start() -> ! {
+    loop{}
 }
