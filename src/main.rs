bc9ef68b108e94b21912a3cec8a5deca37481217
Youngjoon Lee <taxihighway@gmail.com>
2020-12-26 18:21:36 +0900

    Make it run in QEMU

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-#![no_std]
-#![no_main]  // not to use the Rust runtime and crt0
+#![no_std]  // don't link the Rust standard library
+#![no_main]  // disable all Rust-level entry points
 
 use core::panic::PanicInfo;
 
@@ -9,8 +9,21 @@ fn panic(_info: &PanicInfo) -> ! {  // returning the 'never' type
     loop{}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 // Overwrite the entry point
 #[no_mangle] // disabling the name mangling to ensure that Rust compiler really outputs a func name with the name `_start`.
 pub extern "C" fn _start() -> ! {
-    loop{}
+    // This is the entry point, since the linker looks for a function named `_start` by default.
+
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
 }
