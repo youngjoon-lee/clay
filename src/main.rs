935020598fdb6bf34eb12733c6f5bf288b18b62d
Youngjoon Lee <taxihighway@gmail.com>
2020-12-27 18:55:34 +0900

    Implement VGA text buffer

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,21 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 // Called on panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {  // returning the 'never' type
+fn panic(info: &PanicInfo) -> ! {  // returning the 'never' type
+    println!("{}", info);
     loop{}
 }
-
-static HELLO: &[u8] = b"Hello World!";
 
 // Overwrite the entry point
 #[no_mangle] // disabling the name mangling to ensure that Rust compiler really outputs a func name with the name `_start`.
 pub extern "C" fn _start() -> ! {
     // This is the entry point, since the linker looks for a function named `_start` by default.
 
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World{}", "!");
 
     loop {}
 }
